a7cd8043fe9485b980bc6ee888e92c5525fa7cc8
Kamil Preweda <preweda.kamil@o2.pl>
2022-08-07 13:12:20 +0200

    Added HttpGet method that return one SuperHero based on id

--- SuperHeroAPI/Controllers/SuperHeroController.cs
+++ SuperHeroAPI/Controllers/SuperHeroController.cs
@@ -7,10 +7,7 @@ namespace SuperHeroAPI.Controllers
     [ApiController]
     public class SuperHeroController : ControllerBase
     {
-        [HttpGet]
-        public async Task<IActionResult> Get()
-        {
-            var heroes = new List<SuperHero>
+        private static List<SuperHero> heroes = new List<SuperHero>
             {
                 new SuperHero
                 {
@@ -19,9 +16,35 @@ namespace SuperHeroAPI.Controllers
                     FirstName = "Peter",
                     LastName = "Parker",
                     Place = "New York City"
+                },
+                new SuperHero
+                {
+                    Id = 2,
+                    Name = "IronMan",
+                    FirstName = "Tony",
+                    LastName = "Stark",
+                    Place = "Long Island"
                 }
             };
+        [HttpGet]
+        public async Task<ActionResult<List<SuperHero>>> Get()
+        {          
+            return Ok(heroes);
+        }
 
+        [HttpGet("{id}")]
+        public async Task<ActionResult<SuperHero>> Get(int id)
+        {
+            var hero = heroes.Find(h => h.Id == id);
+            if (hero == null)
+                return BadRequest("Hero not found");
+            return Ok(hero);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<List<SuperHero>>> AddHero(SuperHero hero)
+        {
+            heroes.Add(hero);
             return Ok(heroes);
         }
     }
